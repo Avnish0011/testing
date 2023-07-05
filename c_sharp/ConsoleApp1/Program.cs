ce3c0800365347f0b5e31a7d8f287a438938cc97
Avnish0011 <138551463+Avnish0011@users.noreply.github.com>
2023-07-05 06:27:52 +0530

    Update Program.cs

--- c_sharp/ConsoleApp1/Program.cs
+++ c_sharp/ConsoleApp1/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+Console.WriteLine("Enter the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int sum = number1 + number2;
+Console.WriteLine($"The sum of {number1} and {number2} is {sum}.");
