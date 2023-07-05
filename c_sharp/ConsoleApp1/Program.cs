884ed0aab822aaa4269197a01f2677d99e67977d
avnish <1ms20cs147.elective@gmail.com>
2023-07-05 08:54:31 +0530

    xyz

--- c_sharp/ConsoleApp1/Program.cs
+++ c_sharp/ConsoleApp1/Program.cs
@@ -3,5 +3,8 @@ Console.WriteLine("Enter the first number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the second number: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-int sum = number1 + number2;
-Console.WriteLine($"The sum of {number1} and {number2} is {sum}.");
+if(number1==number2){
+
+    Console.WriteLine("Both the numbers are equal!!!");
+}
+Console.ReadKey();
